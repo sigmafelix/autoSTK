084b6b35ff052f2f77159f7eae243e49fbcc3080
sigmafelix <sigmafelix@gmail.com>
2017-04-15 02:24:57 +0900

    Create plot.autofitVariogram.R

---
+++ plot.autofitVariogram.R
@@ -0,0 +1,23 @@
+### plot.autofitVariogram.R
+### Original Author: Paul Hiemstra (paul@numbertheory.nl)
+### Partial fix: Felix Song (henry385@snu.ac.kr)
+### Fix
+#### Easy implementation of the main title
+#### Change of the parameter direction to the numeric vector of length 3
+
+plot.autofitVariogram <- 
+function (x, plotit = TRUE, title="Experimental variogram and fitted variogram model", ...) 
+{
+  shift = 0.03
+  labels = as.character(x$exp_var$np)
+  vario = xyplot(gamma ~ dist, data = x$exp_var, panel = automap:::autokrige.vgm.panel, 
+                 labels = labels, shift = shift, model = x$var_model, 
+                 direction = c(1, 0, 0), 
+                 ylim = c(min(0, 1.04 * min(x$exp_var$gamma)), 1.04 * 
+                            max(x$exp_var$gamma)), xlim = c(0, 1.04 * max(x$exp_var$dist)), 
+                 xlab = "Distance", ylab = "Semi-variance", main = title, 
+                 mode = "direct", ...)
+  if (plotit) 
+    print(vario)
+  else vario
+}
